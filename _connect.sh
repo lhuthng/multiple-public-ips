25d0b8502b501e82b37912e588469a6e09e38cdd
Huu Thang, Le <62278351+lhuthng@users.noreply.github.com>
2024-12-28 09:37:46 +0100

    add renewal

--- _connect.sh
+++ _connect.sh
@@ -3,6 +3,6 @@ warp-cli status
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o CloudflareWARP -j MASQUERADE
 
-sudo iptables -A FORWARD -i A -o CloudflareWARP -j ACCEPT
-sudo iptables -A FORWARD -i B -o CloudflareWARP -j ACCEPT
-sudo iptables -A FORWARD -i C -o CloudflareWARP -j ACCEPT
+sudo iptables -A FORWARD -i ens38 -o CloudflareWARP -j ACCEPT
+sudo iptables -A FORWARD -i ens39 -o CloudflareWARP -j ACCEPT
+sudo iptables -A FORWARD -i ens40 -o CloudflareWARP -j ACCEPT
---
+++ _renew.sh
@@ -0,0 +1,2 @@
+warp-cli registration delete
+warp-cli registration new
