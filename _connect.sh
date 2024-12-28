87dca967a608ad19f583e76912737d1ec405d208
Huu Thang, Le <62278351+lhuthng@users.noreply.github.com>
2024-12-28 08:28:12 +0100

    add forwarding rules

--- _connect.sh
+++ _connect.sh
@@ -2,3 +2,7 @@ warp-cli connect
 warp-cli status
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -o CloudflareWARP -j MASQUERADE
+
+sudo iptables -A FORWARD -i A -o CloudflareWARP -j ACCEPT
+sudo iptables -A FORWARD -i B -o CloudflareWARP -j ACCEPT
+sudo iptables -A FORWARD -i C -o CloudflareWARP -j ACCEPT
